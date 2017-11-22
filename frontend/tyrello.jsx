b23b924e6d0544e28fa148f22b35d19f47225e76
Nicholas <nwilliams770@gmail.com>
2017-11-21 16:32:49 -0800

    Add User Table, Model, Controller

---
+++ frontend/tyrello.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Root from './components/root';
+// import configureStore from './store/store';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Started From the Bottom Now We Here</h1>, root);
+});
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/tyrello.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'es2016', 'jsx']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
