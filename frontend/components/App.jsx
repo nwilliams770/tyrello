e03b14dd23e95e1333139b3930bc05d3a21a9902
Nicholas <nwilliams770@gmail.com>
2017-11-21 21:26:49 -0800

    Debug app and root components

---
+++ frontend/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+const App = () => (
+  <div>
+    <h1> I Just Wanna Know </h1>
+  </div>
+);
+
+export default App;
--- frontend/tyrello.jsx
+++ frontend/tyrello.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Root from './components/root';
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
   const root = document.getElementById('root');
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  ReactDOM.render(<h1>Started From the Bottom Now We Here</h1>, root);
+
+  ReactDOM.render(<Root store={ store}/>, root);
 });
