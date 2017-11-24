f603d37f012d31411ac8f0c367a944484f2c2363
Nicholas <nwilliams770@gmail.com>
2017-11-24 10:14:57 -0800

    Prep for production

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -12,16 +12,14 @@ import SignupFormContainer from './signup_form/signup_form_container';
 
 import BoardsIndexContainer from './boards_index/boards_index_container';
 
-import { AuthRoute, BoardsRoute } from '../util/route_util';
+import { AuthRoute } from '../util/route_util';
 
 const App = () => (
   <div>
-
     <Switch>
       <AuthRoute path="/signup" component={SignupFormContainer} />
       <Route path="/boards" component={BoardsIndexContainer} />
       <AuthRoute path="/" component={SplashPage} />
-
     </Switch>
   </div>
 );
--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -12,19 +12,10 @@ const Auth = ({component: Component, path, loggedIn }) => (
     )}/>
 );
 
-// const Boards = ({component: Component, path, loggedIn }) => (
-//   <Route path={path} render={(props) => (
-//       loggedIn? (
-//         <Component {...props} />
-//       ) : (
-//         <Redirect to="/boards" />
-//       )
-//     )}/>
-// );
+
 
 const mapStateToProps = state => {
   return {loggedIn: Boolean(state.session.currentUser)};
 };
 
 export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
-// export const BoardsRoute = withRouter(connect(mapStateToProps, null)(Boards));
