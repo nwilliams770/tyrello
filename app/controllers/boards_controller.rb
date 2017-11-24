1ae420ce39ee55f026f9c542883f47c36a9773e0
Nicholas <nwilliams770@gmail.com>
2017-11-24 09:53:57 -0800

    Finish Boards controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,39 @@
 class BoardsController < ApplicationController
+  def create
+    @board = Board.new(board_params)
+    if @board.save!
+      render :json
+    else
+      render :json @board.errors.full_messages, status: 422
+    end
+  end
+
+  def index
+    @board = Board.find(params[:id])
+    render :index
+  end
+
+  def show
+    @board = Board.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+    if @board.save!
+      render :json
+    else
+      render :json @board.errors.full_messages, status: 422
+  end
+
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy!
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :starred)
+  end
 end
