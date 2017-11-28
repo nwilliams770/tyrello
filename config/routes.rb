1d485e613a82d276ad7471e0f6bc11e232cdf599
Nicholas <nwilliams770@gmail.com>
2017-11-28 11:43:51 -0800

    add routes for Lists and Cards

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy]
     resources :boards, except: [ :new ]
+    resources :lists, only: [:create, :destroy, :update]
+    resources :cards, only: [:create, :destroy, :update]
   end
   root "static_pages#root"
 end
